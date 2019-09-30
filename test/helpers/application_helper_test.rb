0e7e2180267a929c4ad367e9e4eb20134df7e378
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-09-30 20:36:00 +0200

    Fresh install

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test 'full title' do
-    assert_equal full_title, 'Ruby on Rails Tutorial Sample App'
-    assert_equal full_title('Help'), 'Help | Ruby on Rails Tutorial Sample App'
-  end
-end
